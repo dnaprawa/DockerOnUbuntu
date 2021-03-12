eeac2efc763249937a74ea875fcd0f3cab04b713
Administrator <admin@example.com>
2021-03-12 09:16:28 +0100

    Updated docker-compose version

--- docker.sh
+++ docker.sh
@@ -6,5 +6,5 @@ sudo sh get-docker.sh
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
